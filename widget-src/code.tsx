1827edb634d816da25df72237a39ed4136e5da5d
Elynn Lee <elynnlee@utexas.edu>
2023-07-05 19:54:43 -0700

    very basic functionality, unstyled

--- widget-src/code.tsx
+++ widget-src/code.tsx
@@ -40,11 +40,11 @@ function Button({
 function TeammatePhotoBubble({
   figmaUser,
   isActive = false,
-  onUserSelected = undefined,
-}: {
+}: // onUserSelected = undefined,
+{
   figmaUser: User;
   isActive?: boolean;
-  onUserSelected?: (user: User) => void;
+  // onUserSelected?: (user: User) => void;
 }) {
   const photoUrl = figmaUser.photoUrl;
   const teammateName = figmaUser.name;
@@ -82,7 +82,61 @@ function TeammatePhotoBubble({
   );
 }
 
+function TeammatePhotoBubbleRow({
+  user1 = undefined,
+  user2 = undefined,
+  user3 = undefined,
+}: // onUserSelected,
+{
+  key?: any;
+  user1?: [User | undefined, number | undefined];
+  user2?: [User | undefined, number | undefined];
+  user3?: [User | undefined, number | undefined];
+  // onUserSelected: (user: User) => void;
+}) {
+  return (
+    <AutoLayout
+      direction={"horizontal"}
+      verticalAlignItems="center"
+      horizontalAlignItems="center"
+      spacing={30}
+      padding={{ horizontal: 50 }}
+    >
+      {user1 && user1[0] ? (
+        <TeammatePhotoBubble
+          figmaUser={user1[0]}
+          // hasGone={user1[1]}
+          // onUserSelected={onUserSelected}
+        />
+      ) : null}
+      {user2 && user2[0] ? (
+        <TeammatePhotoBubble
+          figmaUser={user2[0]}
+          // hasGone={user2[1]}
+          // onUserSelected={onUserSelected}
+        />
+      ) : null}
+      {user3 && user3[0] ? (
+        <TeammatePhotoBubble
+          figmaUser={user3[0]}
+          // hasGone={user3[1]}
+          // onUserSelected={onUserSelected}
+        />
+      ) : null}
+    </AutoLayout>
+  );
+}
+
 function Widget() {
+  // List of active users
+  const [users, setUsers] = useSyncedState<User[]>("users", () => {
+    // uncomment for testing
+    // return TEST_ACTIVE_USERS;
+
+    // TODO: initialize activeUsers here instead
+    return [];
+  });
+
   const [activeTeammate, setActive] = useSyncedState<User | null>(
     "activeTeammate",
     null
@@ -90,16 +144,37 @@ function Widget() {
 
   // order to display users
   const userIdToDisplayOrder = useSyncedMap<number>("displayOrder");
+  // map user Ids to User object
+  const userIdToUser = useSyncedMap<User>("idToUser");
 
   const addUserToDisplay = () => {
-    // update map + say this person has gone
-    // userIdToDisplayOrder.set(user.id, userIdToDisplayOrder.size + 1);
-    console.log("hello");
     const currentUser = figma.currentUser;
-    if (currentUser) {
-      console.log(currentUser.id);
+    if (
+      currentUser &&
+      currentUser.id &&
+      !userIdToDisplayOrder.get(currentUser.id)
+    ) {
+      let orderNum = userIdToDisplayOrder.size + 1;
+      userIdToDisplayOrder.set(currentUser.id, orderNum);
+      userIdToUser.set(currentUser.id, currentUser);
     }
   };
+
+  const printNames = () => {
+    const keys = userIdToDisplayOrder.keys();
+    console.log(keys);
+    for (var i = 0; i < keys.length; i++) {
+      let user = userIdToDisplayOrder.get(keys[i]);
+      console.log(user);
+      console.log(userIdToUser.get(keys[i]));
+      let userObj = userIdToUser.get(keys[i]);
+      if (userObj) {
+        return <TeammatePhotoBubble figmaUser={userObj} />;
+      }
+    }
+  };
+
+  const userFromId = (userId: string) => {};
 
   return (
     <AutoLayout
@@ -119,11 +194,40 @@ function Widget() {
         padding={{ top: 0, left: 0, right: 0, bottom: 15 }}
       >
         <Button
-          text={"Next (random)"}
+          text={"Add me"}
           onClick={() => {
             addUserToDisplay();
           }}
         />
+      </AutoLayout>
+      <AutoLayout direction={"vertical"} spacing={20}>
+        {printNames()}
+
+        <AutoLayout></AutoLayout>
+
+        {/* {userIdToDisplayOrder.map((user, idx, users) => {
+          if (idx % 3 != 0) {
+            return null;
+          }
+          var user1hasGone = users[idx]
+            ? userIdToDisplayOrder.get(users[idx].id)
+            : undefined;
+          var user2hasGone = users[idx + 1]
+            ? userIdToDisplayOrder.get(users[idx + 1].id)
+            : undefined;
+          var user3hasGone = users[idx + 2]
+            ? userIdToDisplayOrder.get(users[idx + 2].id)
+            : undefined;
+
+          return (
+            <TeammatePhotoBubbleRow
+              key={idx}
+              user1={[users[idx], user1hasGone]}
+              user2={[users[idx + 1], user2hasGone]}
+              user3={[users[idx + 2], user3hasGone]}
+            />
+          );
+        })} */}
       </AutoLayout>
     </AutoLayout>
   );
